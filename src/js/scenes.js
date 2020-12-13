106cfc7fb0311f39dbd468d44e63c56a4d0ba204
MrSergio99 <73499688+MrSergio99@users.noreply.github.com>
2020-12-13 20:25:16 +0100

    Menú implementación

--- src/js/scenes.js
+++ src/js/scenes.js
@@ -130,7 +130,6 @@ class BossRoom extends BaseScene {
         this.gorila = new GreatGorila(this, 500, 96, 'greatGorila');
         this.parrot = new Parrot(this, 650, 175, 'greatParrot');
 
-
         if (!firstTimeBoss) {
             this.player0.x = this.dungeonDoor.x - 80;
             this.player1.x = this.dungeonDoor.x - 48;
@@ -146,33 +145,36 @@ class BossRoom extends BaseScene {
             firstTimeBoss = false;
         }
 
-        //Dependiendo del número de bosses derrotados se activa el siguiente boss
-        switch (defeatedBosses) {
-            case 0:
-                if (hasRelic) {
-                    this.dungeonDoor.Close();
-                    this.currentBoss = this.gorila;
-                    this.gorila.WakeUp();
-                    hasRelic = false;
-                }
-                break;
-            case 1:
-                if (hasRelic) {
-                    this.dungeonDoor.Close();
-                    this.currentBoss = this.parrot;
-                    this.parrot.WakeUp();
-                    hasRelic = false;
-                }
-                this.gorila.setFrame(27);
-                break;
+        if (hasRelic) {
+            this.dungeonDoor.Close();
 
-            default:
-                break;
+            //Dependiendo del número de bosses derrotados se activa el siguiente boss
+            switch (defeatedBosses) {
+                case 0:
+                    if (hasRelic) {
+                        this.dungeonDoor.Close();
+                        this.currentBoss = this.gorila;
+                        this.gorila.WakeUp();
+                        hasRelic = false;
+                    }
+                    break;
+                case 1:
+                    if (hasRelic) {
+                        this.dungeonDoor.Close();
+                        this.currentBoss = this.parrot;
+                        this.parrot.WakeUp();
+                        hasRelic = false;
+                    }
+                    this.gorila.setFrame(27);
+                    break;
+
+                default:
+                    break;
+            }
+
+            hasRelic = false;
         }
-
-        hasRelic = false;
     }
-
 
     UpdateStage() {
         if (this.currentBoss && !this.currentBoss.awake) {
@@ -348,6 +350,9 @@ class MainMenu extends Phaser.Scene {
         );
 
         this.load.image('title', '/resources/img/Interfaz/Menu/Title.png');
+        this.load.image('newGame', '/resources/img/Interfaz/Menu/Buttons1.png');
+        this.load.image('credits', '/resources/img/Interfaz/Menu/Buttons2.png');
+
         this.load.image('menuBackground', '/resources/img/Interfaz/Menu/menuBackground.png');
     }
 
@@ -357,35 +362,57 @@ class MainMenu extends Phaser.Scene {
         this.camera = this.cameras.main;
         this.EnableFullScreen();
 
-        this.text = this.add.text(240, 250, 'CLICK TO START', {
-            fontFamily: '"CambriaB"',
-            fontSize: '12px'
-        }).setOrigin(0.5).setDepth(10); //, stroke: '0f0f0f', strokeThickness: 20
 
-        this.title = this.add.image(240, 40, 'title').setOrigin(0.5, 0.5).setDepth(10);
+
+        this.title = this.add.image(170, 40, 'title').setOrigin(0.5, 0.5).setDepth(10);
         this.bg = this.add.sprite(240, 135, 'intro').setOrigin(0.5, 0.5);
 
+        this.newGame = this.add.image(65, 90, 'newGame').setOrigin(0.5, 0.5).setDepth(10).setInteractive();
+            this.credits = this.add.image(65, 130, 'credits').setOrigin(0.5, 0.5).setDepth(10).setInteractive();
 
-        this.input.on('pointerdown', function (event) {
-
-            if (this.step == 0) {
-                this.step++;
-                this.bg.setFrame(this.step);
-                this.title.destroy();
-                this.text.destroy();
-
-            } else if (this.step < this.steps) {
-                this.step++;
-                this.bg.setFrame(this.step);
-
-            } else {
-                this.step = 0;
-                this.scene.start('altarRoom');
-            }
+        
 
 
+            this.newGame.on('pointerdown', function (event) {
+                if (this.step == 0) {
+                    this.step++;
+                    this.bg.setFrame(this.step);
+                    this.title.destroy();
+                    this.newGame.destroy();
+                    this.credits.destroy();
 
-        }, this);
+
+                    this.text = this.add.text(240, 260, 'CLICK TO CONTINUE', {
+                        fontFamily: '"CambriaB"',
+                        fontSize: '10px'
+                    }).setOrigin(0.5).setDepth(10);; //, stroke: '0f0f0f', strokeThickness: 20
+                    this.input.on('pointerdown', function (event) {
+                        if (this.step == 0) {
+                            this.step++;
+                            this.bg.setFrame(this.step);
+                        } else if (this.step < this.steps) {
+                            this.step++;
+                            this.bg.setFrame(this.step);
+                        } else {
+                            this.step = 0;
+                            this.scene.start('altarRoom');
+                        }
+
+                    }, this);
+
+
+                }
+
+            }, this);
+
+
+            this.credits.on('pointerdown', function (event) {
+
+                this.scene.start('Credits');
+
+            }, this);
+
+        
     }
 
     update(time, delta) { }
